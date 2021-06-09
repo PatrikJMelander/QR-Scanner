bc0558b364559b871ee15f4dd6876c1cef673675
PatrikJMelander <patrikjmelander@gmail.com>
2021-06-09 09:59:32 +0200

    Just some small fix

--- src/JS/qrCodeReader.js
+++ src/JS/qrCodeReader.js
@@ -1,4 +1,4 @@
-const qrcode = window.qrcode;
+//const qrcode = window.qrcode; already defined
 
 const video = document.createElement("video");
 const canvasElement = document.getElementById("qr-canvas");
