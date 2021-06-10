cf59da16b6060ad2bd285dd7c99de9e54b4aaf6c
PatrikJMelander <patrikjmelander@gmail.com>
2021-06-10 14:52:58 +0200

    modified export to excel

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,24 +1,23 @@
-var wb = XLSX.utils.table_to_book(document.getElementById('tableToExcel'), {sheet:"Scanned References"});
-wb.Props = {
-    Title: "Scans",
-    Subject: "Scan",
-    Author: "Patrik Melander",
-    CreatedDate: new Date(2021,06,09)
-};
+$('#create-excel-btn').click(function(){
+    var wb = XLSX.utils.table_to_book(document.getElementById('tableToExcel'), {sheet:"Scanned References"});
+    wb.Props = {
+        Title: "Scans",
+        Subject: "Scan",
+        Author: "Patrik Melander",
+        CreatedDate: new Date(2021,06,09)
+    };
 
+    var wbout = XLSX.write(wb, {bookType:'xlsx', type: 'binary'});
 
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+      }
+    
+    
 
-
-var wbout = XLSX.write(wb, {bookType:'xlsx', type: 'binary'});
-
-
-function s2ab(s) {
-    var buf = new ArrayBuffer(s.length);
-    var view = new Uint8Array(buf);
-    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
-    return buf;
-  }
-  $('#create-excel-btn').click(function(){
     saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'scans.xlsx');
 });
 
